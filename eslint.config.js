57aea5f5617d63544c3d768b38fdfbd7dc2e5062
Kenyatta Naji Johnson-Adams <k.naji.ja@gmail.com>
2026-03-17 21:44:21 +0900

    fix: FOR FUCK'S SAKE

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginN from 'eslint-plugin-n';
-import vitest from 'eslint-plugin-vitest';
+import vitest from '@vitest/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
